d4c37ccdfc6f3a8a39f27e8f7518ab5f323070d7
cf <carter.feld.man@gmail.com>
2024-06-25 04:04:33 +0800

    fix wallet main

--- apps/wallet/src/main.tsx
+++ apps/wallet/src/main.tsx
@@ -4,13 +4,11 @@ import App from "./App";
 import "./index.css";
 
 import '@mantine/core/styles.css';
-import * as dogeJs from "@qstudio/doge-js";
 
 import {
   RouterProvider,
 } from "react-router-dom";
 import { MantineProvider } from '@mantine/core';
-(window as any).dg = dogeJs;
 import { router } from './routes';
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
