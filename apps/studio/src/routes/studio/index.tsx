4f2dba536fabaf6c755dcc91218dd588bc12e541
cf <carter.feld.man@gmail.com>
2024-07-31 11:47:25 +0800

    updated qstudio

--- apps/studio/src/routes/studio/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const StudioPage: React.FC = ()=>{
-  return(
-    <div>Studio</div>
-  )
-}
-
-export default StudioPage;
--- packages/bar/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { meaningOfLife } from "@qstudio/foo";
-
-// eslint-disable-next-line no-console
-console.log(meaningOfLife);
