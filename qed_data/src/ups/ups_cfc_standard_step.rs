c750f9596c760e6793980fa2387196c8c0efa98d
carter <carter@carter.at>
2025-02-02 13:21:30 +0800

    added deferred_tx circuit

--- qed_data/src/ups/ups_cfc_standard_step.rs
+++ qed_data/src/ups/ups_cfc_standard_step.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 
 
 
-use super::{ups_standard_cfc_input::UPSVerifyCFCStandardStepInput, verify_previous_ups_step::VerifyPreviousUPSStepProofInProofTreeInput};
+use super::{ups_standard_cfc_input::{UPSVerifyCFCStandardStepInput, UPSVerifyPopDeferredTxStepInput}, verify_previous_ups_step::VerifyPreviousUPSStepProofInProofTreeInput};
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash)]
 #[serde(bound = "for<'de2> F: Deserialize<'de2>")]
@@ -18,6 +18,27 @@ pub struct UPSCFCStandardTransactionCircuitInput<F: RichField> {
 
 
 impl<F: RichField> KVQSerializable for UPSCFCStandardTransactionCircuitInput<F> {
+    fn to_bytes(&self) -> anyhow::Result<Vec<u8>> {
+        bincode::serialize(self).map_err(|e| anyhow::anyhow!(e))
+    }
+
+    fn from_bytes(bytes: &[u8]) -> anyhow::Result<Self> {
+        bincode::deserialize(bytes).map_err(|e| anyhow::anyhow!(e))
+    }
+}
+
+
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash)]
+#[serde(bound = "for<'de2> F: Deserialize<'de2>")]
+pub struct UPSCFCDeferredTransactionCircuitInput<F: RichField> {
+    pub verify_previous_ups_step: VerifyPreviousUPSStepProofInProofTreeInput<F>,
+    pub deferred_tx_cfc_step: UPSVerifyPopDeferredTxStepInput<F>,
+}
+
+
+
+impl<F: RichField> KVQSerializable for UPSCFCDeferredTransactionCircuitInput<F> {
     fn to_bytes(&self) -> anyhow::Result<Vec<u8>> {
         bincode::serialize(self).map_err(|e| anyhow::anyhow!(e))
     }
--- qed_rollup_circuit/src/ups/circuits/mod.rs
+++ qed_rollup_circuit/src/ups/circuits/mod.rs
@@ -1,2 +1,3 @@
 pub mod ups_start;
 pub mod ups_cfc_standard;
+pub mod ups_cfc_deferred_tx;
