02aaa3c23ceb6976f9c7302f80444c0d8e4f3ba8
jason3 <hellojason328@gmail.com>
2025-03-11 15:17:20 +0800

    fix: match error when deal with u32

--- qed-interpreter/src/lib.rs
+++ qed-interpreter/src/lib.rs
@@ -1364,27 +1364,19 @@ impl<F: ContextFelt + From<u32>, C: DPNContext<F> + 'static> Interpreter<F, C> {
         scrutinee_value: &CheckedValueRef<F>,
     ) -> Result<F> {
         let pattern_value = self.interpret_expr(program, *pattern_expr, ctx)?;
-        let pattern_borrow = pattern_value.borrow();
-        let scrutinee_borrow = scrutinee_value.borrow();
+        let (scrutinee_borrow, pattern_borrow) = (scrutinee_value.borrow(), pattern_value.borrow());
 
-        let ret = match (&*scrutinee_borrow, &*pattern_borrow) {
-            (CheckedValue::Felt(l), CheckedValue::Felt(r)) => self.context.op_eq(*l, *r),
-            (CheckedValue::U32(l), CheckedValue::U32(r)) => {
-                //todo!: need add op_eq for u32
-                let const_0 = self.context.op_const_u32(0);
-                let sub = self.context.op_u32_xor(*l, *r);
-                if const_0 == sub {
-                    self.context.op_const(1)
-                }else {
-                    self.context.op_const(0)
-                }
-            },
-            (CheckedValue::Bool(l), CheckedValue::Bool(r)) => self.context.op_eq(*l, *r),
-            _ => unreachable!(),
-        };
-
-        Ok(ret)
+        match (&*scrutinee_borrow, &*pattern_borrow) {
+            (CheckedValue::Felt(l), CheckedValue::Felt(r))
+            | (CheckedValue::U32(l), CheckedValue::U32(r))
+            | (CheckedValue::Bool(l), CheckedValue::Bool(r)) => Ok(self.context.op_eq(*l, *r)),
+            _ => unreachable!(
+                "Unsupported match pattern between {:?} and {:?}",
+                scrutinee_borrow, pattern_borrow
+            ),
+        }
     }
+
     pub fn is_constant(&self, value: F) -> bool {
         let constant_types = [
             DPNOpType::Constant,
