4f7d234e002cfcfc3a767f2d187fd8de5fe38d8f
fjchen7 <fjchen7@gmail.com>
2025-03-28 16:25:30 +0800

    Implement compile_cmd

--- qed-dargo-cli/src/compile_cmd.rs
+++ qed-dargo-cli/src/compile_cmd.rs
@@ -1,7 +1,11 @@
-use crate::errors::CliError;
+use crate::Result;
 use clap::Args;
 use qed_dargo::workspace::Workspace;
-
+use qed_interpreter::Interpreter;
+use qedlang_core::dpn::{
+    ops::{exec_context::QExecContext, sym_felt::SymFeltRef},
+    vm::compile::QEDCompileResult,
+};
 /// Compile the program and its secret execution trace into ACIR format
 #[derive(Debug, Clone, Args)]
 pub(crate) struct CompileCommand {
@@ -9,9 +13,8 @@ pub(crate) struct CompileCommand {
     compile_options: CompileOptions,
 }
 
-pub(crate) fn run(args: CompileCommand, workspace: Workspace) -> Result<(), CliError> {
+pub(crate) fn run(args: CompileCommand, workspace: Workspace) -> Result<()> {
     compile_workspace_full(&workspace, &args.compile_options)?;
-
     Ok(())
 }
 
@@ -20,10 +23,33 @@ pub(crate) fn run(args: CompileCommand, workspace: Workspace) -> Result<(), CliE
 pub(super) fn compile_workspace_full(
     workspace: &Workspace,
     compile_options: &CompileOptions,
-) -> Result<(), CliError> {
+) -> anyhow::Result<()> {
+    let entry_manager = crate::resolve_entries(workspace);
+    let mut interpreter = Interpreter::<SymFeltRef, _>::new(QExecContext::new());
+    let compile_results = interpreter.interpret(
+        entry_manager.entry,
+        entry_manager.dependencies_entries.into_iter().collect(),
+        compile_options.contract_name.clone(),
+        compile_options.method_names.clone(),
+        |context, (method_name, method_id, outputs)| {
+            QEDCompileResult::compile_exec(
+                method_name,
+                method_id,
+                &context.store,
+                &context,
+                &outputs,
+            )
+        },
+    )?;
+    println!("compile_result: {:?}", compile_results);
     Ok(())
 }
 
 /// Options for the compile command
 #[derive(Args, Clone, Debug, Default)]
-pub struct CompileOptions {}
+pub struct CompileOptions {
+    #[clap(short, env, long, default_value = None)]
+    contract_name: Option<String>,
+    #[clap(short, env, long, num_args = 1.., default_values = &["main"])]
+    method_names: Vec<String>,
+}
--- qed-dargo-cli/src/lib.rs
+++ qed-dargo-cli/src/lib.rs
@@ -1,13 +1,15 @@
 mod compile_cmd;
 mod errors;
 
-use crate::errors::CliError;
 use clap::Args;
-use qed_dargo::fm::NormalizePath;
 use qed_dargo::workspace::Workspace;
 use qed_dargo_toml::files::{find_file_manifest_root, get_package_manifest};
 use qed_dargo_toml::resolve_workspace_from_toml;
 use std::path::PathBuf;
+use std::collections::HashSet;
+use qed_dargo::package::Dependency;
+
+pub(crate) type Result<T> = anyhow::Result<T>;
 
 #[derive(Args, Clone, Debug)]
 pub(crate) struct DargoConfig {
@@ -21,7 +23,7 @@ pub(crate) struct DargoConfig {
 }
 
 /// Parses a path and turns it into an absolute one by joining to the current directory.
-fn parse_path(path: &str) -> Result<PathBuf, String> {
+fn parse_path(path: &str) -> std::result::Result<PathBuf, String> {
     use qed_dargo::fm::NormalizePath;
     let mut path: PathBuf = path
         .parse()
@@ -32,9 +34,9 @@ fn parse_path(path: &str) -> Result<PathBuf, String> {
     Ok(path)
 }
 
-fn with_workspace<C, R>(cmd: C, config: DargoConfig, run: R) -> Result<(), CliError>
+fn with_workspace<C, R>(cmd: C, config: DargoConfig, run: R) -> Result<()>
 where
-    R: FnOnce(C, Workspace) -> Result<(), CliError>,
+    R: FnOnce(C, Workspace) -> Result<()>,
 {
     // All commands need to run on the workspace level, because that's where the `target` directory is.
     let package_dir = find_file_manifest_root(&config.program_dir)?;
@@ -43,4 +45,43 @@ where
     let mut workspace = resolve_workspace_from_toml(&toml_path)?;
     workspace.target_dir = config.target_dir.clone();
     run(cmd, workspace)
+}
+
+#[derive(Clone)]
+pub struct EntryManager {
+    entry: PathBuf,
+    dependencies_entries: HashSet<PathBuf>,
+}
+
+impl EntryManager {
+    pub fn new(entry: PathBuf) -> Self {
+        Self {
+            entry,
+            dependencies_entries: HashSet::new(),
+        }
+    }
+
+    pub fn add_dependency_entry(&mut self, entry: PathBuf) -> bool {
+        self.dependencies_entries.insert(entry)
+    }
+}
+
+fn resolve_entries(workspace: &Workspace) -> EntryManager {
+    let package = workspace.package.clone();
+    let package_entry_path = package.entry_canonical_path();
+    let mut entry_manager = EntryManager::new(package_entry_path);
+    let mut package_stack = vec![package];
+    while let Some(package) = package_stack.pop() {
+        for dep in package.dependencies.values() {
+            match dep {
+                Dependency::Remote { package } | Dependency::Local { package } => {
+                    let entry_path = package.entry_canonical_path();
+                    if entry_manager.add_dependency_entry(entry_path) {
+                        package_stack.push(package.clone());
+                    }
+                }
+            }
+        }
+    }
+    entry_manager
 }
