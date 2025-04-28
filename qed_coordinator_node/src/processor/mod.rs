3b407c30dadd362245dc547f9b8b04ab7d2358b1
zilong-dai <2840478265@qq.com>
2025-04-28 16:38:20 +0800

    fix

--- qed_coordinator_node/src/processor/mod.rs
+++ qed_coordinator_node/src/processor/mod.rs
@@ -33,9 +33,6 @@ use qed_store::{
 };
 use std::{ sync::Arc, time::Duration};
 
-use crate::args::CoordinatorProcessorArgs;
-use crate::COORDINATOR_WORKER_SUFFIX;
-
 type C = PoseidonGoldilocksConfig;
 const D: usize = 2;
 type F = QEDFelt;
