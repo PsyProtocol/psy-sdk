6cada2e277436ee815b45475c929e80f6af1d019
0x8f701 <alex_cj96@foxmail.com>
2025-05-12 14:43:36 +0800

    merge working proof
    Signed-off-by: 0x8f701 <alex_cj96@foxmail.com>

--- qed_node_common/src/verifier/cached_common_data.rs
+++ qed_node_common/src/verifier/cached_common_data.rs
@@ -15,7 +15,7 @@ where
 {
     let serialized_library =
         serde_json::from_str::<SerializedGenericCircuitCommonDataLibraryInfo>(
-            r#"{"common_data_hashes":["acd2052c5f2d49c2a931d3c8545876e51ce9c42ca1f7713d46cdac5e2e0f7f19","70724c5b671675652c93e738c2ec3296b6366994fae960f747f03c2156720c11","7ea492b556ffc919706ac50511cadc6c6b3b38afe4810c7b5ef3320652b50f0e"],"common_circuit_list":[[14,7,15,8,12,10,11,9,226,13],[32,18,1,48,0,227,19,54],[6,228]]}"#
+            r#"{"common_data_hashes":["acd2052c5f2d49c2a931d3c8545876e51ce9c42ca1f7713d46cdac5e2e0f7f19","70724c5b671675652c93e738c2ec3296b6366994fae960f747f03c2156720c11","7ea492b556ffc919706ac50511cadc6c6b3b38afe4810c7b5ef3320652b50f0e"],"common_circuit_list":[[14,10,13,7,12,11,226,15,8,9],[48,18,227,19,0,1,54,32],[228,6]]}"#
         ).unwrap();
 
     GenericCircuitCommonDataLibrary::<C, D>::from_serialized(
