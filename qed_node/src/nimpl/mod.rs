4aeaedf4ecd4f2a8ef83f4a205bb486e626a42f1
clint-long <clint@zklabs.cn>
2025-05-16 15:34:03 +0800

    Add async Redis drain queue implementation

--- qed_node/src/nimpl/mod.rs
+++ qed_node/src/nimpl/mod.rs
@@ -8,6 +8,7 @@ use fred::types::{ClusterHash, CustomCommand};
 
 pub mod drain_queue_fred;
 pub mod drain_queue_redis;
+pub mod drain_queue_redis_async;
 pub mod proof_store_fred;
 pub mod proof_store_redis_async;
 pub mod proof_store_redis;
