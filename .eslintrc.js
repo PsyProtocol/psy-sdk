c371c3820b4a34c5606e9fd2785184268ae1b332
clint <clint@zklabs.cn>
2025-05-21 11:13:59 +0800

    refactor(city-wallet-widget): update exports and remove unused dependencies
    - Remove unused exports from index.tsx
    - Delete styles.d.ts file
    - Remove @qstudio/blokies-react dependency
    - Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,8 @@
+// .eslintrc.js
 module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'commonjs'
+    }
 };
--- packages/city-wallet-widget/src/index.tsx
+++ packages/city-wallet-widget/src/index.tsx
@@ -1,6 +1,3 @@
-export type { IQCityWidgetWallet } from './types';
-export { CityWalletWidget } from './components/CityWalletWidget';
-export { useWalletState } from './hooks/useWalletState';
 export type { IWalletWidgetRPC } from './utils/rpc/types';
 export * from './utils/rpc/walletRPC';
 
