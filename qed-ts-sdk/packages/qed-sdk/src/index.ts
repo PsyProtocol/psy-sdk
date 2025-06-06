42864bfc212c9c3d0721c6a6abdbbababd98aab4
clint-long <clint@zklabs.cn>
2025-06-06 10:37:10 +0800

    remove unused constants and simplify exports

--- qed-ts-sdk/packages/qed-sdk/src/index.ts
+++ qed-ts-sdk/packages/qed-sdk/src/index.ts
@@ -1,41 +1,17 @@
-export type { ISimpleHTTPRequest, ISimpleHTTPResponse, IHTTPClient } from "./http";
-export { FetchHTTPClient } from "./http";
-export type {
-    QedHash,
-    Felt,
-    SCNumberLike,
-    U8Bytes,
-    PrivateKey,
-    PublicKey,
-    QHashOut,
-    Hash256,
-    Hash160,
-    CompressedPublicKeyHex,
-    QProvingJobDataIDSerializedWrapped,
-    QedMerkleProof,
-    QedDeltaMerkleProof,
-    ISimpleKVPair,
-    DeltaMerkleProofCore,
-    MerkleProofCore,
-} from "./core";
-
+export * from "./http";
+export * from "./core";
 export * from "./utils/felt";
 
 // Enhanced RPC Provider exports
-export {
-    Provider,
-    type CacheConfig,
-    type RetryConfig,
-    type MultiProviderConfig,
-    type ClientConfig,
-    type ProviderHealth,
-} from "./provider";
+export * from "./provider";
 
 // Coordinator Edge RPC exports
-export { CoordinatorEdgeRpcProvider, ICoordinatorEdgeRpcProvider } from "./coord-edge-rpc";
+export * from "./coord-edge-rpc";
 
 // Realm Edge RPC exports
-export { RealmEdgeRpcProvider, IRealmEdgeRpcProvider } from "./realm-edge-rpc";
+export * from "./realm-edge-rpc";
 
 // QED User Prover RPC exports
-export { QEDRPCUserProverProvider, IQEDUserProverProvider } from "./local-prover-rpc";
+export * from "./local-prover-rpc";
+
+export * from "./wallet";
