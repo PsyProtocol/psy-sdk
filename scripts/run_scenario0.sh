d5714838adadd2e9f200228b97f843f8a463c2d4
clint-long <clint@zklabs.cn>
2025-08-07 11:09:36 +0800

    optimize block building and sleep intervals

--- scripts/run_scenario0.sh
+++ scripts/run_scenario0.sh
@@ -55,43 +55,34 @@ trap cleanup SIGINT SIGTERM
 
 # Execute the scenario commands
 log_message "Starting Scenario 0..."
-sleep 15
 
 run_make_command "make deploy-contract" "Deploy Contract"
 run_make_command "make register-user" "Register User"
 run_make_command "make build-block" "Build Block 1"
-sleep 15
-run_make_command "make build-block" "Build Block 2"
-sleep 15
+# wait for block 2 to be committed
+sleep 30s
 run_make_command "make mint" "Mint"
-run_make_command "make build-block" "Build Block 3"
-sleep 15
-run_make_command "make build-block" "Build Block 4"
-sleep 15
+run_make_command "make build-block" "Build Block 2"
+# wait for block 2 to be committed
+sleep 30s
 
 get_user_token_info
 
 run_make_command "make transfer" "Transfer"
-run_make_command "make build-block" "Build Block 5"
-sleep 15
-run_make_command "make build-block" "Build Block 6"
-sleep 15
+run_make_command "make build-block" "Build Block 3"
+sleep 30s
 
 get_user_token_info
 
 run_make_command "make claim" "Claim"
-run_make_command "make build-block" "Build Block 7"
-sleep 15
-run_make_command "make build-block" "Build Block 8"
-sleep 15
+run_make_command "make build-block" "Build Block 4"
+sleep 6
 
 get_user_token_info
 
 run_make_command "make return-back" "return-back"
-run_make_command "make build-block" "Build Block 9"
-sleep 15
-run_make_command "make build-block" "Build Block 10"
-sleep 15
+run_make_command "make build-block" "Build Block 5"
+sleep 6
 
 get_user_token_info
 
