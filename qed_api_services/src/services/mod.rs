96fe20442acf90e5d6006cc88c3347979884ee29
jimzk <jimzkx@gmail.com>
2025-08-22 16:20:44 +0800

    implement api service main

--- qed_api_services/src/services/mod.rs
+++ qed_api_services/src/services/mod.rs
@@ -1,14 +1,24 @@
 use crate::config::Config;
+use crate::websocket::WebSocketManager;
 use sqlx::PgPool;
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
 #[derive(Clone)]
 pub struct ApiService {
     pub pool: PgPool,
+    pub websocket_manager: WebSocketManager,
 }
 
 impl ApiService {
     pub fn new(pool: PgPool) -> Self {
-        Self { pool }
+        Self {
+            pool,
+            websocket_manager: WebSocketManager {
+                connections: Arc::new(RwLock::new(HashMap::new())),
+            },
+        }
     }
 }
 
