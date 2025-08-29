678411e3e0dcb7463417d88825c4846189133423
jimzk <jimzkx@gmail.com>
2025-08-29 13:40:20 +0800

    feat: add script to startup api-services

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -23,6 +23,8 @@ WORKER0_LOG="$LOG_DIR/worker0.log"
 WORKER1_LOG="$LOG_DIR/worker1.log"
 WORKER2_LOG="$LOG_DIR/worker2.log"
 
+API_SERVICES_LOG="$LOG_DIR/api-services.log"
+
 # LOCAL_USER_PROVER_LOG="$LOG_DIR/local-user-prover.log"
 LOCAL_PROVE_PROXY_LOG="$LOG_DIR/local-prove-proxy.log"
 WEB_WALLET_LOG="$LOG_DIR/web_wallet.log"
@@ -40,6 +42,7 @@ echo "Clearing log files..."
 : > "$WORKER0_LOG"
 : > "$WORKER1_LOG"
 : > "$WORKER2_LOG"
+: > "$API_SERVICES_LOG"
 
 # Array to store PIDs of background processes
 declare -a PIDS=()
@@ -98,6 +101,9 @@ PIDS+=($!)
 run_service "make run-realm-edge" "realm-edge" "$REALM_EDGE_LOG" &
 PIDS+=($!)
 run_service "make run-realm-edge1" "realm-edge1" "$REALM_EDGE1_LOG" &
+PIDS+=($!)
+
+run_service "make run-api-services" "api-services" "$API_SERVICES_LOG" &
 PIDS+=($!)
 
 sleep 1
