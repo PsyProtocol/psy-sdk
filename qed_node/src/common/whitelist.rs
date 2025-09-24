934acd198d9d81a11827e57e41b249a0c95a98ae
0x8f701 <alex_cj96@foxmail.com>
2025-09-24 12:39:16 +0800

    add whitelist cache to reload whitelist every 30 seconds
    Signed-off-by: 0x8f701 <alex_cj96@foxmail.com>

--- qed_node/src/common/whitelist.rs
+++ qed_node/src/common/whitelist.rs
@@ -7,8 +7,10 @@ use qed_prover::wallet::secp_sign::{Eip712Signable, SignedRequest};
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::Path;
-use std::time::{SystemTime, UNIX_EPOCH};
-use tracing::info;
+use std::sync::{Arc, RwLock};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tokio::task::JoinHandle;
+use tracing::{info, error};
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct WhitelistConfig {
@@ -88,5 +90,67 @@ impl WhiteList {
         }
 
         Ok(())
+    }
+}
+
+#[derive(Clone)]
+pub struct WhiteListCache {
+    config_path: String,
+    whitelist: Arc<RwLock<WhiteList>>,
+    _reload_task: Arc<JoinHandle<()>>,
+}
+
+impl WhiteListCache {
+    pub fn new<P: AsRef<Path>>(config_path: P) -> Result<Self> {
+        let path_str = config_path.as_ref().to_string_lossy().to_string();
+        let initial_whitelist = WhiteList::from_file(&config_path)?;
+        let whitelist = Arc::new(RwLock::new(initial_whitelist));
+
+        let whitelist_clone = Arc::clone(&whitelist);
+        let path_clone = path_str.clone();
+
+        let reload_task = tokio::spawn(async move {
+            let mut interval = tokio::time::interval(Duration::from_secs(30));
+            loop {
+                interval.tick().await;
+
+                match WhiteList::from_file(&path_clone) {
+                    Ok(new_whitelist) => {
+                        if let Ok(mut cache) = whitelist_clone.write() {
+                            *cache = new_whitelist;
+                            info!("Successfully reloaded whitelist from config");
+                        } else {
+                            error!("Failed to acquire write lock for whitelist cache");
+                        }
+                    }
+                    Err(e) => {
+                        error!("Failed to reload whitelist from config: {}", e);
+                    }
+                }
+            }
+        });
+
+        Ok(Self {
+            config_path: path_str,
+            whitelist,
+            _reload_task: Arc::new(reload_task),
+        })
+    }
+
+    pub fn get_whitelist(&self) -> Arc<RwLock<WhiteList>> {
+        Arc::clone(&self.whitelist)
+    }
+
+    pub fn verify_request<T>(
+        &self,
+        request: &SignedRequest<QHashOut<GoldilocksField>>,
+        original_data: &T,
+        expiry_duration: Option<Duration>,
+    ) -> Result<()>
+    where
+        T: serde::Serialize,
+    {
+        let whitelist = self.whitelist.read().map_err(|_| anyhow::anyhow!("Failed to acquire read lock"))?;
+        whitelist.verify_request(request, original_data, expiry_duration)
     }
 }
