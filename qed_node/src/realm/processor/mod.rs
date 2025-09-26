75139c8e3ebafa47e15c118a4e280fb4b8358c8a
clint-long <clint@zklabs.cn>
2025-09-26 14:03:20 +0800

    fix rollback logic

--- qed_node/src/realm/processor/mod.rs
+++ qed_node/src/realm/processor/mod.rs
@@ -255,7 +255,7 @@ impl RealmProcessor {
             if ret.latest_checkpoint_id < checkpoint || ret.checkpoint_id > checkpoint + 10 {
                 warn!("Rollback: invalid checkpoint sync result, latest_checkpoint_id: {}, checkpoint: {}", ret.latest_checkpoint_id, checkpoint);
                 build_ctx.rollback(checkpoint).await?;
-                self.pending_checkpoint_id.swap(0, Ordering::Relaxed);
+                self.pending_checkpoint_id.store(0, Ordering::Relaxed);
             }
         }
         Ok(())
