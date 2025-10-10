e9be7960e6d2d5d7437cb09fa1444e6522166014
clint-long <clint@zklabs.cn>
2025-10-10 16:32:38 +0800

    fix(realm): adjust checkpoint sync logic and logging

--- qed_node/src/realm/processor/mod.rs
+++ qed_node/src/realm/processor/mod.rs
@@ -229,8 +229,8 @@ impl RealmProcessor {
                 COORDINATOR_USER_TREE_HEIGHT,
                 self.realm_config.realm_id as u64,
             ).await?;
-            trace!("pending checkpoint id: {}, latest checkpoint id: {}, local realm root: {}, remote realm root: {}",
-                checkpoint, ret.latest_checkpoint_id, realm_root.value, ret.realm_root);
+            trace!("pending checkpoint id: {}, synced checkpoint id：{}, latest checkpoint id: {}, local realm root: {}, remote realm root: {}",
+                checkpoint, ret.checkpoint_id, ret.latest_checkpoint_id, realm_root.value, ret.realm_root);
 
             if ret.checkpoint_id == checkpoint && realm_root.value == ret.realm_root {
                 let (pair_to_set, remove_keys) = build_ctx.commit(ret.checkpoint_id).await?;
@@ -249,10 +249,8 @@ impl RealmProcessor {
                         error!("❌ Failed to send realm backup request for checkpoint {}: {}", checkpoint, e);
                     }
                 }
-            }
-            let checkpoint = self.pending_checkpoint_id.load(Ordering::Relaxed);
-            if ret.latest_checkpoint_id < checkpoint || (checkpoint > 0 && ret.checkpoint_id > checkpoint) {
-                warn!("Rollback: invalid checkpoint sync result, latest_checkpoint_id: {}, checkpoint: {}", ret.latest_checkpoint_id, checkpoint);
+            } else {
+                warn!("Rollback: invalid checkpoint sync result, latest_checkpoint_id: {}, pending checkpoint id: {}, synced checkpoint id：{}", ret.latest_checkpoint_id, checkpoint, ret.checkpoint_id);
                 build_ctx.rollback(checkpoint).await?;
                 self.pending_checkpoint_id.store(0, Ordering::Relaxed);
             }
