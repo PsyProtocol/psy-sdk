ccbf3e3f910190287b5c875f335ad90a4149ac4a
zilong-dai <2840478265@qq.com>
2025-10-14 16:15:06 +0800

    serialize QEDBasicZKSignatureCircuit circuit_data

--- qed_common_circuit/src/circuits/zk_signature3/core.rs
+++ qed_common_circuit/src/circuits/zk_signature3/core.rs
@@ -4,9 +4,9 @@ use plonky2::{
         circuit_data::{CircuitConfig, CircuitData, CommonCircuitData, VerifierOnlyCircuitData},
         config::{AlgebraicHasher, GenericConfig},
         proof::ProofWithPublicInputs,
-    }
+    }, util::serialization::{DefaultGateSerializer, DefaultGeneratorSerializer}
 };
-use qed_core::data::qhashout::QHashOut;
+use qed_core::{data::qhashout::QHashOut, utils::debug_timer::DebugTimer};
 use qed_crypto::{common::witnesses::zk_signature::QEDZKSignatureCircuitInput, hash::traits::hasher::MerkleZeroHasher, signature::zk::wallet::PRIVATE_KEY_CONSTANTS};
 
 use crate::{
@@ -40,6 +40,9 @@ where
 {
     pub fn new() -> Self {
         let config = CircuitConfig::standard_recursion_zk_config();
+
+        let mut timer = DebugTimer::new("new zk sign circuit");
+        timer.lap("build inner circuit");
         let mut builder = CircuitBuilder::<C::F, D>::new(config);
 
         let private_key = builder.add_virtual_hash();
@@ -83,9 +86,10 @@ where
         );
         builder.register_public_inputs(&public_inputs_hash.elements);
         let circuit_data = builder.build::<C>();
+        timer.lap("build inner circuit done");
 
         // start add some gates to make it easier to integrate with others
-
+        timer.lap("build minifier circuit");
         let added_gates_for_minifier = [
             GateRef::new(ComparisonGate::new(32, 16)),
         ];
@@ -96,6 +100,7 @@ where
             2,
             Some(&added_gates_for_minifier),
         );
+        timer.lap("build minifier circuit done");
         let fingerprint = QHashOut(minifier_chain.get_fingerprint());
         Self {
             private_key,
@@ -115,6 +120,100 @@ where
         pw.set_hash_target(self.sig_hash, sig_hash.0)?;
         let inner_proof = self.circuit_data.prove(pw)?;
         self.minifier_chain.prove(&inner_proof)
+    }
+}
+
+impl<C: GenericConfig<D> + Default, const D: usize> QEDBasicZKSignatureCircuit<C, D>
+where
+    C::Hasher:AlgebraicHasher<C::F>,
+{
+    pub fn serialize_circuit_data(&self) -> anyhow::Result<Vec<u8>> {
+        let gate_serializer = DefaultGateSerializer;
+        let generator_serializer = DefaultGeneratorSerializer::<C, D>::default();
+
+        let data_bytes = self.circuit_data
+            .to_bytes(&gate_serializer, &generator_serializer)
+            .map_err(|_| anyhow::Error::msg("CircuitData serialization failed."))?;
+
+        Ok(data_bytes)
+    }
+
+    pub fn new_with_file(file_path: &str) -> anyhow::Result<Self> {
+        let bytes = std::fs::read(file_path)?;
+        Self::new_with_serialized_circuit(&bytes)
+    }
+
+    pub fn new_with_serialized_circuit(circuit_data_bytes: &[u8]) -> anyhow::Result<Self> {
+        let config = CircuitConfig::standard_recursion_zk_config();
+        let mut timer = DebugTimer::new("new zk sign circuit with serialized data");
+        timer.lap("build inner circuit");
+        let mut builder = CircuitBuilder::<C::F, D>::new(config);
+
+        let private_key = builder.add_virtual_hash();
+        let private_key_constants = PRIVATE_KEY_CONSTANTS
+            .iter()
+            .map(|c| builder.constant(C::F::from_canonical_u64(*c)))
+            .collect::<Vec<_>>();
+        let public_key_param_target = builder.hash_n_to_hash_no_pad::<C::Hasher>(vec![
+            private_key_constants[0],
+            private_key_constants[1],
+            private_key_constants[2],
+            private_key_constants[19],
+            private_key.elements[1],
+            private_key_constants[1],
+            private_key_constants[2],
+            private_key_constants[3],
+            private_key_constants[4],
+            private_key_constants[5],
+            private_key_constants[6],
+            private_key.elements[0],
+            private_key_constants[7],
+            private_key.elements[2],
+            private_key_constants[8],
+            private_key_constants[9],
+            private_key_constants[10],
+            private_key_constants[11],
+            private_key_constants[12],
+            private_key.elements[3],
+            private_key_constants[13],
+            private_key_constants[14],
+            private_key_constants[15],
+            private_key_constants[16],
+            private_key_constants[17],
+            private_key_constants[18],
+        ]);
+        let sig_hash = builder.add_virtual_hash();
+
+        // let circuit_data = builder.build::<C>();
+        let gate_serializer = DefaultGateSerializer;
+        let generator_serializer = DefaultGeneratorSerializer::<C, D>::default();
+        let circuit_data = CircuitData::<C::F, C, D>::from_bytes(&circuit_data_bytes, &gate_serializer, &generator_serializer)
+            .map_err(|e| anyhow::format_err!("CircuitData deserialization failed. Error: {}", e))?;
+
+        timer.lap("build inner circuit done");
+
+        // start add some gates to make it easier to integrate with others
+        timer.lap("build minifier circuit");
+        let added_gates_for_minifier = [
+            GateRef::new(ComparisonGate::new(32, 16)),
+        ];
+
+        let minifier_chain = QEDProofMinifierChain::<D, C::F, C>::new_add_gates(
+            &circuit_data.verifier_only,
+            &circuit_data.common,
+            2,
+            Some(&added_gates_for_minifier),
+        );
+        timer.lap("build minifier circuit done");
+        let fingerprint = QHashOut(minifier_chain.get_fingerprint());
+
+        Ok(Self {
+            private_key,
+            sig_hash,
+            circuit_data,
+            minifier_chain: minifier_chain,
+            fingerprint,
+        })
     }
 }
 impl<C: GenericConfig<D>, const D: usize> QStandardCircuit<C, D> for QEDBasicZKSignatureCircuit<C, D>
@@ -143,5 +242,62 @@ where
         input: &QEDZKSignatureCircuitInput<C::F>,
     ) -> anyhow::Result<ProofWithPublicInputs<C::F, C, D>> {
         self.prove_base(input.private_key, input.sig_hash)
+    }
+}
+
+mod test {
+    use plonky2::plonk::config::PoseidonGoldilocksConfig;
+    use qed_core::utils::debug_timer::DebugTimer;
+
+    use super::*;
+    
+    const D: usize = 2;
+    type C = PoseidonGoldilocksConfig;
+    type F = <C as GenericConfig<D>>::F;
+
+    #[test]
+    fn test_serialize_circuit() -> anyhow::Result<()> {
+        let mut timer = DebugTimer::new("serialize_circuit test");
+        timer.lap("init zk sign circuit");
+        let zk_circuit = QEDBasicZKSignatureCircuit::<C, D>::new();
+        timer.lap("init zk sign circuit done");
+
+        timer.lap("serialize circuit data");
+        let circuit_data_bytes = zk_circuit.serialize_circuit_data()?;
+        timer.lap("serialize circuit data done");
+        
+        timer.lap("deserialize circuit data");
+        let zk_circuit2 = QEDBasicZKSignatureCircuit::<C, D>::new_with_serialized_circuit(&circuit_data_bytes)?;
+        timer.lap("deserialize circuit data done");
+        assert_eq!(zk_circuit.fingerprint, zk_circuit2.fingerprint);
+        assert_eq!(zk_circuit.circuit_data, zk_circuit2.circuit_data);
+        assert_eq!(zk_circuit.private_key, zk_circuit2.private_key);
+        assert_eq!(zk_circuit.sig_hash, zk_circuit2.sig_hash);
+
+        let input = QEDZKSignatureCircuitInput {
+            private_key: QHashOut::rand(),
+            sig_hash: QHashOut::rand(),
+        };
+        let mut pw = PartialWitness::new();
+        pw.set_hash_target(zk_circuit2.private_key, input.private_key.0)?;
+        pw.set_hash_target(zk_circuit2.sig_hash, input.sig_hash.0)?;
+        
+        timer.lap("prove inner");
+        let inner_proof = zk_circuit2.circuit_data.prove(pw)?;
+        timer.lap("prove inner done");
+
+        timer.lap("prove minifier");
+        let proof2 = zk_circuit.minifier_chain.prove(&inner_proof)?;
+        timer.lap("prove minifier done");
+
+        timer.lap("verify inner proof");
+        zk_circuit2.circuit_data.verify(inner_proof)?;
+        timer.lap("verify inner proof done");
+
+        timer.lap("verify minifier proof");
+        zk_circuit.minifier_chain.verify(proof2)?;
+        timer.lap("verify minifier proof done");
+
+        Ok(())
     }
 }
