651ccddef3f723b73a7c5bcbc11cb425579cd4dd
0x8f701 <alex_cj96@foxmail.com>
2025-10-28 13:06:57 +0800

    remove qed_core
    Signed-off-by: 0x8f701 <alex_cj96@foxmail.com>

--- qed_core/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![feature(min_specialization)]
-pub mod data;
-pub mod config;
-pub mod traits;
-pub mod utils;
-pub mod job;
-pub mod ups;
--- qed_core/src/traits/qwitnessable.rs
+++
@@ -1,3 +0,0 @@
-pub trait QWitnessable<T> {
-    fn set_q_witness(&mut self, witness: &T);
-}
