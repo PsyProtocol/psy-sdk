59272ceb938962a09f5c9abdc7993b66f43bad4a
zilong-dai <2840478265@qq.com>
2025-11-18 16:12:00 +0800

    impl IContractProvider for PsyUserWalletProvider

--- psy_sdk/psy-ts-sdk/packages/psy-sdk/src/wallet/provider.ts
+++ psy_sdk/psy-ts-sdk/packages/psy-sdk/src/wallet/provider.ts
@@ -1,12 +1,27 @@
+import { Felt } from "../core";
 import { getPsyNetworkMagicForNetworkId, NetworkId } from "../action";
-import { ICoordinatorEdgeRpcProvider } from "../coord-edge-rpc";
-import { IPsyUserProverProvider } from "../local-prover-rpc";
-import { IRealmEdgeRpcProvider } from "../realm-edge-rpc";
-import { IPsyTransactionSignerProvider } from "../zksigner";
+import { ICoordinatorEdgeRpcProvider, MultiCoordinatorRpcProvider } from "../coord-edge-rpc";
+import { ContractCallData, IPsyUserProverProvider } from "../local-prover-rpc";
+import { IRealmEdgeRpcProvider, MultiRealmRpcProvider } from "../realm-edge-rpc";
+import { IPsyTransactionSignerProvider, PsyMemoryTransactionSignerProvider } from "../zksigner";
 import { IPsyUserWallet, IPsyUserWalletProvider } from "./types";
 import { PsyUserWallet } from "./userWallet";
+import { PsyNetworkConfig } from "../config";
+import { initWasmSync, PsyWasmWebProverProvider, WasmRpcServer } from "../local-web-prover";
+import { PsyJSON } from "../utils";
 
-class PsyUserWalletProvider implements IPsyUserWalletProvider {
+export interface IContractProvider {
+    getContractState(checkpointId: Felt, contractId: Felt, userId: Felt, slots: Felt[]): Promise<Felt[]>;
+
+    sendTransaction(
+        contractId: Felt,
+        functionName: string,
+        args: any[],
+        publicKey: string
+    ): Promise<any>;
+}
+
+class PsyUserWalletProvider implements IPsyUserWalletProvider, IContractProvider {
     networkId: NetworkId;
     l2NetworkMagic: bigint;
     signerProvider: IPsyTransactionSignerProvider;
@@ -54,6 +69,83 @@ class PsyUserWalletProvider implements IPsyUserWalletProvider {
                 )
         );
     }
+
+    async getContractState(
+        checkpointId: Felt,
+        contractId: Felt,
+        userId: Felt,
+        slots: Felt[]
+    ): Promise<Felt[]> {
+        const userStateTreeHeight = (await this.coordinatorEdgeRpcProvider.getContractLeafData(contractId)).state_tree_height;
+        if (!Array.isArray(slots)) {
+            throw new Error(`slots must be an array, got ${typeof slots}`);
+        }
+        const slotValues = await this.realmEdgeRpcProvider.getRpcProviderByUserId(userId).getSlotValues(checkpointId, userId, contractId, Number(userStateTreeHeight), slots);
+        return slotValues;
+    }
+
+    async sendTransaction(
+        contractId: Felt,
+        functionName: string,
+        args: any[],
+        publicKey: string
+    ): Promise<any> {
+        const signer = await this.signerProvider.getSignerByPublicKeyHex(publicKey);
+        const contractCallData: ContractCallData = {
+            contract_calls: [
+                {
+                    contract_id: BigInt(contractId),
+                    method_name: functionName,
+                    inputs: args.map((arg) => BigInt(arg))
+                }
+            ]
+        };
+        return signer.signAndSubmit(publicKey, contractCallData);
+    }
 }
 
-export { PsyUserWalletProvider };
+async function createMemoryWalletProvider(
+    config: PsyNetworkConfig,
+): Promise<PsyUserWalletProvider> {
+    const networkId = "regtest";
+    const coordinator_rpc = new MultiCoordinatorRpcProvider(
+        config.coordinator_configs,
+    );
+    const realm_rpc = new MultiRealmRpcProvider(
+        config.realm_configs,
+        config.users_per_realm,
+    );
+
+    const initWasmRpcServer = async () => {
+        try {
+            const json = PsyJSON.stringify(config);
+            const now = new Date().getTime();
+            initWasmSync();
+            PsyWasmWebProverProvider.wasmServer = await new WasmRpcServer(json);
+            console.log(`WASM initialized in ${(new Date().getTime() - now) / 1000} seconds`);
+        } catch (error) {
+            console.error('Failed to get prover URL:', error);
+        }
+    };
+    await initWasmRpcServer();
+
+    let userProver: IPsyUserProverProvider = new PsyWasmWebProverProvider(config);
+    console.log("User Prover:", userProver);
+    console.log("User Prover:", PsyWasmWebProverProvider.wasmServer);
+
+
+    const transactionSignerProvider = new PsyMemoryTransactionSignerProvider(
+        userProver,
+        networkId,
+    );
+
+    return new PsyUserWalletProvider(
+        networkId,
+        coordinator_rpc,
+        realm_rpc,
+        transactionSignerProvider,
+        userProver,
+    );
+}
+
+export { PsyUserWalletProvider, createMemoryWalletProvider };
