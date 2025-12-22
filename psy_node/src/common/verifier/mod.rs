35bbf1650800b57cac48388d0c2420974a5efc6a
zilong-dai <2840478265@qq.com>
2025-12-22 13:40:22 +0800

    update endcap verify data

--- psy_node/src/common/verifier/mod.rs
+++ psy_node/src/common/verifier/mod.rs
@@ -13,3 +13,35 @@ where
         common: get_cached_common_data_library(),
     }
 }
+
+mod tests {
+    use plonky2::{
+        field::goldilocks_field::GoldilocksField,
+        plonk::{circuit_data::VerifierOnlyCircuitData, config::PoseidonGoldilocksConfig},
+    };
+    use psy_common::{data::alt::AltVerifierOnlyCircuitData, job::id::ProvingJobCircuitType};
+    use psy_crypto::common::{
+        cached_circuit_library::get_cached_circuit_library,
+        circuit_library::{CircuitInfoLibrary, CircuitInfoLibraryCore},
+    };
+
+    type C = PoseidonGoldilocksConfig;
+    const D: usize = 2;
+    type F = GoldilocksField;
+
+    #[test]
+    fn test_get_cached_generic_verifier() -> anyhow::Result<()> {
+        let circuit_library = get_cached_circuit_library::<F>();
+        let endcap_data: VerifierOnlyCircuitData<C, D> = circuit_library.get_verifier_data(ProvingJobCircuitType::UserEndCap)?;
+        let endcap_fingerprint = circuit_library.get_fingerprint(ProvingJobCircuitType::UserEndCap)?;
+
+        let alt_endcap_data = AltVerifierOnlyCircuitData::from(endcap_data);
+
+        println!("endcap_data: {}", serde_json::to_string_pretty(&alt_endcap_data)?);
+        println!("endcap_data: {}", serde_json::to_string(&alt_endcap_data)?);
+        println!("endcap_fingerprint: {:?}", endcap_fingerprint);
+        println!("endcap_fingerprint: {}", serde_json::to_string(&endcap_fingerprint)?);
+
+        Ok(())
+    }
+}
