b5eef54ecf67e87d30cb002ac5c3c14b12785508
zilong-dai <2840478265@qq.com>
2026-01-08 11:56:33 +0800

    use serde default for events field in SubmitUserEndCapNonProofInput

--- psy_core/psy_data/src/guta/end_cap_input.rs
+++ psy_core/psy_data/src/guta/end_cap_input.rs
@@ -25,7 +25,7 @@ use crate::{
 pub struct SubmitUserEndCapNonProofInput<F: RichField> {
     pub core: SubmitUserEndCapNonProofCoreInput<F>,
     pub contract_state_updates: Vec<PsyContractStateUpdateHistory<F>>,
-    #[serde(skip)]
+    #[serde(default)]
     pub events: Vec<PsyUserEventRecord<F>>,
 }
 impl<F: RichField> SubmitUserEndCapNonProofInput<F> {
